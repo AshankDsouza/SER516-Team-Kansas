bccc19e576d228e33b49038020769cb9d8a7e80b
Araviraj8 <112653403+Araviraj8@users.noreply.github.com>
2024-02-06 01:47:42 -0700

    US#4: Task: #48. Setup springboot for backend

---
+++ src/test/java/com/kansas/TaigaAPI/TaigaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kansas.TaigaAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaigaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
