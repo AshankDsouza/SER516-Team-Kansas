eb71da5d4119f5ce55c521d2e740cb85eff18611
Pratima <psing161@asu.edu>
2024-02-11 19:38:57 -0700

    Task36 and Task56

--- src/main/java/com/kansas/TaigaAPI/controller/TaigaAPI.java
+++ src/main/java/com/kansas/TaigaAPI/controller/TaigaAPI.java
@@ -3,12 +3,13 @@ package com.kansas.TaigaAPI.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @RestController
 @RequestMapping("/api")
 public class TaigaAPI {
     @GetMapping("/")
-    public String hello(){
+    public String hello() {
         return "Hello";
     }
 
@@ -21,4 +22,16 @@ public class TaigaAPI {
     public String getClosedTasksPerWeek() {
         return "Closed tasks per week data";
     }
+
+    @GetMapping("/selectProjectsForMetrics")
+    public String[] getProjectsForMetrics(@RequestParam String metric) {
+        if (metric.equals("burndown")) {
+            String[] projects = {"nicolasrmz-devgym", "whrisurdicky-sim2023q2-spanners",
+                    "the-princess-bride", "a21asmchaben-project-hunt", "vitao-locacao-de-veiculos"};
+            return projects;
+        }
+        return null;
+    }
+
+
 }
