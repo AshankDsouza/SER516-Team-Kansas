68a929cc58768d73b0aad4ed3439599d147cd4f9
ShashankReddyBand <sbanda8@asu.edu>
2024-02-11 20:09:06 -0700

    NextJS setup

---
+++ client/actions/userToken.ts
@@ -0,0 +1,5 @@
+"use server"
+
+export default async function getUserToken(userName:string, password:string) {
+    const response = fetch("localhost:8080/test");
+}
---
+++ client/app/login/page.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
+import { useForm } from "react-hook-form"
+import { Button } from "@/components/ui/button"
+import {
+    Form,
+    FormControl,
+    FormDescription,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+
+const formSchema = z.object({ "name": z.string().min(1).max(255), "password": z.string().min(1).max(255) })
+
+export default function LoginForm() {
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            name: "",
+            password: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
+    return (
+        <div className="flex p-16">
+            <Form {...form}>
+                <form noValidate onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+
+                    <FormField
+                        control={form.control}
+                        name="name"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>UserName</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="userName " {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    Taiga userName
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+
+                    <FormField
+                        control={form.control}
+                        name="password"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Password</FormLabel>
+                                <FormControl>
+                                    <Input placeholder="Password" {...field} />
+                                </FormControl>
+                                <FormDescription>
+                                    Taiga password
+                                </FormDescription>
+                            </FormItem>
+                        )}
+                    />
+                    <Button type="submit">Submit</Button>
+                </form>
+            </Form>
+        </div>
+    )
+}
