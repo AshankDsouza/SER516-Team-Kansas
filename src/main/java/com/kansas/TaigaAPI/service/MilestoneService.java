3269c8e6a2b2170fbcf60f64c6e1735715f1c088
sukanyarani <ranisukanya7777@gmail.com>
2024-02-13 19:31:03 -0700

    Api implemented to return sprints for given project

--- src/main/java/com/kansas/TaigaAPI/service/MilestoneService.java
+++ src/main/java/com/kansas/TaigaAPI/service/MilestoneService.java
@@ -32,7 +32,7 @@ public class MilestoneService {
 
             return objectMapper.readTree(responseJson).get("days");
         } catch(Exception e) {
-            log.error(e.getMessage());
+          //  log.error(e.getMessage());
             return null;
         }
     }
@@ -49,7 +49,7 @@ public class MilestoneService {
 
             return objectMapper.readTree(responseJson);
         } catch(Exception e) {
-            log.error(e.getMessage());
+           // log.error(e.getMessage());
             return null;
         }
     }
