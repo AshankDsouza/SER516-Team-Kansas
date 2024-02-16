8085c64c83a67e24ced2ade66d1e41aeac299b24
ShashankReddyBand <sbanda8@asu.edu>
2024-02-16 16:48:50 -0700

    Implemented visulization of burndown chart

--- client/actions/project.ts
+++ client/actions/project.ts
@@ -1,6 +1,7 @@
 "use server"
 
 import { cookies } from "next/headers";
+import { getRequestOptions } from "./auth";
 
 export async function getProjectId(projectSlug: string) {
     var myHeaders = new Headers();
@@ -31,7 +32,6 @@ export async function getProjectMilestones(projectId: string) {
     const response = await fetch("http://localhost:8080/api/milestones/getAllSprints?project=1521719", requestOptions)
     let sprintIDs = await response.json()
     sprintIDs = Object.keys(sprintIDs).map(key => ({ id: sprintIDs[key], value: key }));
-    console.log(sprintIDs);
     return sprintIDs   
 }
 
@@ -49,4 +49,11 @@ export async function getCyleTime(projectId: string) {
     let cycleTimeData = await response.json()
     console.log({cycleTimeData});
     return cycleTimeData;   
+}
+
+export async function getBurndowMetrics(milestoneId: string) {
+    
+    const response = await fetch(`http://localhost:8080/api/milestones/${milestoneId}/getTotalStoryPoints`, await getRequestOptions())
+    let BurndownData = await response.json()
+    return BurndownData
 }
