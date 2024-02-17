816588f475efded4641fa5cd923ffe47f664bd62
ShashankReddyBand <sbanda8@asu.edu>
2024-02-17 16:36:39 -0700

    Provided sprint filter for users on lead time and cycle time

--- client/actions/project.ts
+++ client/actions/project.ts
@@ -4,39 +4,19 @@ import { cookies } from "next/headers";
 import { getRequestOptions } from "./auth";
 
 export async function getProjectId(projectSlug: string) {
-    var myHeaders = new Headers();
-    const auth_token = cookies().get("auth_token")
-
-    myHeaders.append("Authorization", `Bearer ${auth_token?.value}`);
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders
-    };
-
-    const response = await fetch(`https://api.taiga.io/api/v1/projects/by_slug?slug=${projectSlug}`, requestOptions)
+    const response = await fetch(`https://api.taiga.io/api/v1/projects/by_slug?slug=${projectSlug}`, await getRequestOptions())
     const projectId = await response.json()
     return projectId.id
 }
 
 export async function getProjectMilestones(projectId: string) {
-    var myHeaders = new Headers();
-    const auth_token = cookies().get("auth_token")
-    myHeaders.append("Authorization", `Bearer ${auth_token?.value}`);
-
-    var requestOptions = {
-        method: 'GET',
-        headers: myHeaders
-    };
-
-    const response = await fetch("http://localhost:8080/api/milestones/getAllSprints?project=1521719", requestOptions)
+    const response = await fetch("http://localhost:8080/api/milestones/getAllSprints?project=1521719", await getRequestOptions())
     let sprintIDs = await response.json()
     sprintIDs = Object.keys(sprintIDs).map(key => ({ id: sprintIDs[key], value: key }));
     return sprintIDs
 }
 
 export async function getBurndowMetrics(milestoneId: string) {
-    
     const response = await fetch(`http://localhost:8080/api/milestones/${milestoneId}/getTotalStoryPoints`, await getRequestOptions())
     let BurndownData = await response.json()
     return BurndownData
