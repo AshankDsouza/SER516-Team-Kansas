3b64c1a26c0f8c44e73308703121de8b05ccad6e
Pratima <psing161@asu.edu>
2024-02-16 20:18:29 -0700

    Test cases added for auth

--- src/test/java/com/kansas/TaigaAPI/TaigaApiApplicationTests.java
+++ src/test/java/com/kansas/TaigaAPI/TaigaApiApplicationTests.java
@@ -1,13 +1,89 @@
 package com.kansas.TaigaAPI;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.kansas.TaigaAPI.model.AuthRequest;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.HashMap;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import com.kansas.TaigaAPI.service.AuthenticationService;
+
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+		webEnvironment = SpringBootTest.WebEnvironment.MOCK,
+		classes = TaigaApiApplication.class)
+@AutoConfigureMockMvc
+@TestPropertySource(
+		locations = "classpath:application.properties")
 class TaigaApiApplicationTests {
+	private MockMvc mockMvc;
+
+//	@Mock
+//	private HashMap<String, Product> productCatalog;
+
+	@Autowired
+	private MockMvc mvc;
+
+	@Autowired
+	private ObjectMapper objectMapper;
 
 	@Test
 	void contextLoads() {
 	}
+
+	@Test
+	void login_without_credentials_bad_request() throws Exception{
+
+		mvc.perform(post("/api/auth")
+						.contentType(MediaType.APPLICATION_JSON))
+				.andExpect(status().isBadRequest());
+	}
+
+	@Test
+	void login_with_bad_credentials_access_denied() throws Exception{
+		AuthRequest authRequest = new AuthRequest();
+		authRequest.setPassword("test123");
+		authRequest.setUsername("johndoe");
+		mvc.perform(post("/api/auth")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(authRequest)))
+				.andExpect(status().isForbidden());
+	}
+
+	@Test
+	void login_with_valid_credentials_ok() throws Exception {
+		AuthRequest authRequest = new AuthRequest();
+		authRequest.setUsername("johndoe"); // Enter your valid username
+		authRequest.setPassword("test123"); // Enter your valid user password
+		mvc.perform(post("/api/auth")
+						.contentType(MediaType.APPLICATION_JSON)
+						.content(objectMapper.writeValueAsString(authRequest)))
+				.andExpect(status().isOk());
+	}
+
 
 }
