9f3dc38543c3a0299fc3fee2ec5401b7a4d0b7f5
AshankDsouza <ajdsouz1@asu.edu>
2024-02-17 17:01:13 -0700

    adds cycle time graph

--- client/actions/project.ts
+++ client/actions/project.ts
@@ -34,3 +34,19 @@ export async function getProjectMilestones(projectId: string) {
     console.log(sprintIDs);
     return sprintIDs   
 }
+
+export async function getCyleTime(projectId: string) {
+    var myHeaders = new Headers();
+    const auth_token = cookies().get("auth_token")
+    myHeaders.append("Authorization", `Bearer ${auth_token?.value}`);
+
+    var requestOptions = {
+        method: 'GET',
+        headers: myHeaders
+    };
+
+    const response = await fetch("http://localhost:8080/api/1521719/376621/getCycleTime", requestOptions)
+    let cycleTimeData = await response.json()
+    console.log({cycleTimeData});
+    return cycleTimeData;   
+}
