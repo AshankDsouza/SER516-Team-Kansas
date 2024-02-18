2ca76baaf90a0b61221268ad84cbe71c155f6572
ShashankReddyBand <sbanda8@asu.edu>
2024-02-18 00:46:52 -0700

    Added wrong credentials check

--- client/actions/project.ts
+++ client/actions/project.ts
@@ -1,23 +1,23 @@
 "use server"
 
 import { cookies } from "next/headers";
-import { getRequestOptions } from "./auth";
+import { getRequestOptions } from "@/utils/auth";
 
 export async function getProjectId(projectSlug: string) {
-    const response = await fetch(`https://api.taiga.io/api/v1/projects/by_slug?slug=${projectSlug}`, await getRequestOptions())
+    const response = await fetch(`https://api.taiga.io/api/v1/projects/by_slug?slug=${projectSlug}`, getRequestOptions())
     const projectId = await response.json()
     return projectId.id
 }
 
 export async function getProjectMilestones(projectId: string) {
-    const response = await fetch("http://localhost:8080/api/milestones/getAllSprints?project=1521719", await getRequestOptions())
+    const response = await fetch("http://localhost:8080/api/getAllSprints?project=1521719", getRequestOptions())
     let sprintIDs = await response.json()
     sprintIDs = Object.keys(sprintIDs).map(key => ({ id: sprintIDs[key], value: key }));
     return sprintIDs
 }
 
 export async function getBurndowMetrics(milestoneId: string) {
-    const response = await fetch(`http://localhost:8080/api/milestones/${milestoneId}/getTotalStoryPoints`, await getRequestOptions())
+    const response = await fetch(`http://localhost:8080/api/${milestoneId}/getBurnDownChart`, getRequestOptions())
     let BurndownData = await response.json()
     return BurndownData
 }
