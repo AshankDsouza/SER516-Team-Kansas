ebf65f853b970a1cfa90e16167e4406574b7411b
AshankDsouza <ajdsouz1@asu.edu>
2024-02-18 21:02:46 -0700

    fixes param bug

--- client/actions/project.ts
+++ client/actions/project.ts
@@ -47,7 +47,7 @@ export async function getLeadTime(projectSlug: string, sprintId: string) {
         headers: myHeaders
     };
     //http://localhost:8080/api/getDataForLeadTime?projectSlug=ser516asu-ser516-team-kansas&sprintNo=1
-    const url = `http://localhost:8080/api/getDataForLeadTime?projectSlug=${projectSlug}&sprintNo=1`;
+    const url = `http://localhost:8080/api/getDataForLeadTime?projectSlug=${projectSlug}&sprintId=${sprintId}`;
     console.log({url: url});
 
     
