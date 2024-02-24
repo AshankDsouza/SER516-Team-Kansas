07445eebec2725f066c7d1f6c5742e3a538c563b
Araviraj8 <112653403+Araviraj8@users.noreply.github.com>
2024-02-23 21:33:28 -0700

    velocity calculation

--- src/main/java/com/kansas/TaigaAPI/service/MilestoneService.java
+++ src/main/java/com/kansas/TaigaAPI/service/MilestoneService.java
@@ -1,15 +1,20 @@
 package com.kansas.TaigaAPI.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.kansas.TaigaAPI.model.CompletedPoints;
 import com.kansas.TaigaAPI.utils.GlobalData;
 import com.kansas.TaigaAPI.utils.HTTPRequest;
 import lombok.extern.log4j.Log4j2;
 import org.apache.http.HttpHeaders;
 import org.apache.http.client.methods.HttpGet;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Log4j2
@@ -22,20 +27,8 @@ public class MilestoneService {
             .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
     public JsonNode getBurnDownMetrics(String authToken,int mileStoneId) {
-        try {
-            String endpoint = TAIGA_API_ENDPOINT + "/milestones/" + mileStoneId+"/stats";
-            HttpGet request = new HttpGet(endpoint);
-            request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authToken);
-            request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
-
-            String responseJson = HTTPRequest.sendHttpRequest(request);
-
-            return objectMapper.readTree(responseJson).get("days");
-        } catch(Exception e) {
-
-          //  log.error(e.getMessage());
-            return null;
-        }
+            JsonNode responseJson = getMilestoneStats(authToken,mileStoneId);
+            return responseJson.get("days");
     }
 
     public JsonNode getMilestoneList(String authToken, int projectId) {
@@ -65,4 +58,37 @@ public class MilestoneService {
         }
         return -1;
     }
+
+    public List<CompletedPoints> getMilestoneCompletedPoints(String authToken,int projectId){
+        List<CompletedPoints> completedPointsList = new ArrayList<>();
+        JsonNode milestoneList = getMilestoneList(authToken,projectId);
+        for(JsonNode milestone: milestoneList){
+            if(milestone.get("id") != null){
+                JsonNode milestoneStats = getMilestoneStats(authToken, milestone.get("id").asInt());
+                int completedPoints = 0;
+                if(milestoneStats.get("completed_points").isArray()){
+                    for(JsonNode points : milestoneStats.get("completed_points")) {
+                        completedPoints += points.asInt();
+                    }
+                }
+                completedPointsList.add(new CompletedPoints(String.valueOf(milestone.get("name").asText()), completedPoints));
+            }
+        }
+        return completedPointsList;
+    }
+
+    public JsonNode getMilestoneStats(String authToken, int milestoneId){
+        try {
+            String endpoint = TAIGA_API_ENDPOINT + "/milestones/" + milestoneId+ "/stats";
+            HttpGet request = new HttpGet(endpoint);
+            request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authToken);
+            request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+            return objectMapper.readTree(HTTPRequest.sendHttpRequest(request));
+        } catch(Exception e) {
+            //  log.error(e.getMessage());
+            return null;
+        }
+    }
+
+
 }
