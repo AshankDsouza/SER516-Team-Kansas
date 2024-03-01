fa33663ade63254e670be498fae361a5c170f286
SukanyaRani <srani6@asu.edu>
2024-02-29 23:11:14 -0700

    Adding code for api integration

--- client/components/focusFactor.tsx
+++ client/components/focusFactor.tsx
@@ -1,81 +1,52 @@
-"use client"
-
-
 import { cookies } from "next/headers";
-import {
-  Select,
-  SelectContent,
-  SelectGroup,
-  SelectItem,
-  SelectLabel,
-  SelectTrigger,
-  SelectValue,
-} from "@/components/ui/select"
-import { useEffect, useState } from "react"
+import { useState, useEffect } from "react"
 import BarGraph from "./barGraph"
+import { getProjectMilestones } from "@/actions/project";
 
 
 async function FocusFactor(props: any) {
-  const {slug} = props;
+  const { slug } = props;
   var myHeaders = new Headers();
   const auth_token = cookies().get("auth_token")
   myHeaders.append("Authorization", `Bearer ${auth_token?.value}`);
 
   var requestOptions = {
-      method: 'GET',
-      headers: myHeaders
+    method: 'GET',
+    headers: myHeaders
   };
-
   type sprint = {
     id: string,
     value: string
   }
-  const [sprints, setsprints] = useState<sprint[]>([{ id: '1', value: "sprint-1" }, { id: '2', value: "sprint-2" }, { id: '3', value: "sprint-3" }])
-  const [selectedSprintID, setselectedSprintID] = useState("")
   const [showChart, setShowChart] = useState(false)
+  const [sprints, setsprints] = useState<sprint[]>([{ id: '1', value: "sprint-1" }, { id: '2', value: "sprint-2" }, { id: '3', value: "sprint-3" }])
   const [labels, setLabels] = useState<string[]>([])
-  const [open_points, setopen_points] = useState<number[]>([])
   const [series, setSeries] = useState<any[]>([])
-
-   
-
-  const response = await fetch("http://localhost:8080/api/tasks/workCapacity?projectId=1521719&sprintId=1", requestOptions)
-
- 
-
-  const data = {
-    labels,
-    datasets: [
-      {
-        label: 'Optimal points',
-        data: open_points,
-        borderColor: '#000',
-        backgroundColor: '#666',
-      },
-    ],
-  };
+  useEffect(() => {
+    getProjectMilestones(slug)
+      .then((data: any) => {
+        setsprints(data)
+      })
+  }, [])
+  const response1 = await fetch(`http://localhost:8080/api/${sprints[0].id}/getTotalPoint`, requestOptions)
+  const response2 = await fetch(`http://localhost:8080/api/${sprints[0].id}/getCompletedPoints`, requestOptions)
+  let lab = [];
+  let ser = [];
+  for (let i = 0; i < response1.length; i++) {
+    lab.push(response1[i].sprintName)
+    let focusFactor = response2[i].completedPoints == 0 ? 0 : response1[i].totalPoints / response2[i].completedPoints;
+    ser.push(focusFactor)
+  }
+  setLabels(lab);
+  setSeries(ser);
+  setShowChart(true)
 
   return (
     <div className="flex border-2 border-slate-300 rounded-md divide-x-2">
       <div className="filters flex flex-col divide-y-2">
-        <div className="p-8 font-bold">Filters</div>
-        <div className="p-8">
-          <Select onValueChange={(e) => setselectedSprintID(e)}>
-            <SelectTrigger className="w-[180px]">
-              <SelectValue placeholder="Sprint" />
-            </SelectTrigger>
-            <SelectContent>
-              <SelectGroup>
-                <SelectLabel>Sprints</SelectLabel>
-                {sprints.map(sprint =>
-                  <SelectItem key={sprint.id} value={sprint.id}>{sprint.value}</SelectItem>
-                )}
-              </SelectGroup>
-            </SelectContent>
-          </Select>
-        </div>
+        <div className="p-8 font-bold">Focus Factor</div>
       </div>
-      {showChart ? <BarGraph name="Focus Factor"  labels={labels} series={series}/> : <div className="flex-1 p-16 min-h-50">Loading...</div>}
+      {showChart ? <BarGraph name="Focus Factor" labels={labels} series={series} /> : <div className="flex-1 p-16 min-h-50">Loading...</div>}
     </div>
   )
 }
