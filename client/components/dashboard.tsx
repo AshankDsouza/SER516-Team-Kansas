a4da8829f66f5814c8afa02bdf26b5b165305d37
AshankDsouza <ajdsouz1@asu.edu>
2024-03-01 17:46:53 -0700

    adds boilerplate code for work capacity

--- client/components/dashboard.tsx
+++ client/components/dashboard.tsx
@@ -6,10 +6,12 @@ import Burndown from "./burndown"
 import LeadTime from "./leadtime"
 import CycleTime from "./cycletime"
 import FocusFactor from "./focusFactor"
+import VelocityGraph from "./velocityGraph"
+import WorkCapacityGraph from "./workCapacityGraph"
 
 function Dashboard({ slug }: { slug: string }) {
   const [chart, setChart] = useState("Burndown")
-  const charts = ["Burndown", "Lead time", "Cycle time","Focus Factor", "Velocity"]
+  const charts = ["Burndown", "Lead time", "Cycle time","Focus Factor", "Velocity", "Work Capacity"]
 
   return (
     <div>
@@ -23,6 +25,8 @@ function Dashboard({ slug }: { slug: string }) {
       {chart == "Cycle time" ? <CycleTime slug={slug} /> : <div></div>}
       {chart == "Focus Factor" ? <FocusFactor slug={slug} /> : <div></div>}
       {chart == "Velocity" ? <VelocityGraph slug={slug} /> : <div></div>}
+      {chart == "Work Capacity" ? <WorkCapacityGraph slug={slug} /> : <div></div>}
+
     </div>
   )
 }
