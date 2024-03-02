86a59b4de0fe681808430ec19a4a7f57a582892f
Araviraj8 <112653403+Araviraj8@users.noreply.github.com>
2024-03-01 18:43:07 -0700

    fixed the API for work capacity

--- src/main/java/com/kansas/TaigaAPI/service/MilestoneService.java
+++ src/main/java/com/kansas/TaigaAPI/service/MilestoneService.java
@@ -79,9 +79,11 @@ public class MilestoneService {
     }
 
 
-    public List<CompletedPoints> getMilestoneCompletedPoints(String authToken,int projectId){
-        List<CompletedPoints> completedPointsList = new ArrayList<>();
+    public List<CompletedPoints> getMilestoneCompletedPoints(String authToken,String projectSlug){
+        ProjectService projectService =new ProjectService();
+        int projectId = projectService.getProjectId(authenticationService.getAuthToken(), projectSlug);
         JsonNode milestoneList = getMilestoneList(authToken,projectId);
+        List<CompletedPoints> completedPointsList = new ArrayList<>();
         for(JsonNode milestone: milestoneList){
             if(milestone.get("id") != null){
                 JsonNode milestoneStats = getMilestoneStats(authToken, milestone.get("id").asInt());
