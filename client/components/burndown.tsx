9fd915f44a7d5a03dbb9bf87d85595b739e59c70
AshankDsouza <ajdsouz1@asu.edu>
2024-03-24 17:52:37 -0700

    adds multi sprint comparison feature

--- client/components/burndown.tsx
+++ client/components/burndown.tsx
@@ -1,6 +1,6 @@
 "use client"
 
-import { getBurndowMetrics, getProjectMilestones } from "@/actions/project"
+import { getBurndowMetrics } from "@/actions/project"
 import {
     Select,
     SelectContent,
@@ -11,11 +11,8 @@ import {
     SelectValue,
 } from "@/components/ui/select"
 import { useEffect, useState } from "react"
-import BurndownChart from "./burndownChart"
 import { useRouter } from "next/navigation"
-import Chip from '@mui/material/Chip';
-import LineGraph from "./graphs/lineGraph"
-import Highcharts from "highcharts"
+import MultiLineGraph from "./graphs/multiLineGraph"
 
 
 
@@ -25,78 +22,38 @@ function Burndown({slug, sprints}:{slug: string, sprints:{id: string, value: str
 
     const [selectedSprintID, setselectedSprintID] = useState("")
     const [showChart, setShowChart] = useState(true)
-    const [labels, setLabels] = useState<string[]>([])
-    const [open_points, setopen_points] = useState<number[]>([])
-    const [optimal_points, setoptimal_points] = useState<number[]>([])
-    const [story_points, setstory_points] = useState<number[]>([])
 
-    let sprintNames = sprints.map((sprint) => sprint.value);
+    const [dataPoints, setDataPoints] = useState<number[]>([]);
 
     useEffect(() => {
         getBurndowMetrics(slug)
             .then((data: any) => {
-                console.log({dataFromMulti: data});
 
                 if(!data){
                     router.refresh();
                     return
                 }
                 console.log({dataFromMulti: data});
-                
-                // setShowChart(false);
-                // const daysArray = data.map((item: any) => item.day);
-                // setLabels(daysArray);
-                // const open_points = data.map((item: any) => item.open_points);
-                // setopen_points(open_points);
-                // setShowChart(true);
+                const result = [];
+
+                // Populate result array and labels list
+                Object.entries(data).forEach(([sprintName, sprintData]) => {
+                    const formattedSprintData ={
+                        name: sprintName,
+                        data: sprintData.map(({ day, open_points, optimal_points }) => ([optimal_points, open_points])),
+                        
+                    }
+                    result.push(formattedSprintData);
+                });
+
+                console.log({result});
+                setDataPoints(result);
             })
     }, [selectedSprintID])
 
 
-    const data = {
-        labels,
-        datasets: [
-            {
-                label: 'Open points',
-                data: open_points,
-                borderColor: '#000',
-                backgroundColor: '#000',
-            },
-            {
-                label: 'Optimal points',
-                data: optimal_points,
-                borderColor: '#333',
-                backgroundColor: '#333',
-            },
-            {
-                label: 'Story points',
-                data: story_points,
-                borderColor: '#666',
-                backgroundColor: '#666',
-            },
-        ],
-    };
-
-    const series = [{
-
-        marker: {
-            fillColor: 'transparent',
-        },
-        data: [7.0, 6.9, 9.5, 14.5, 18.2, 21.5, 25.2, 26.5, 23.3, 18.3, 13.9, 9.6]
-    }, {
-
-        marker: {
-            fillColor: 'transparent'
-        },
-        data: [-0.2, 0.8, 5.7, 11.3, 17.0, 22.0, 24.8, 24.1, 20.1, 14.1, 8.6, 2.5]
-    }, {
-
-        data: [-0.9, 0.6, 3.5, 8.4, 13.5, 17.0, 18.6, 17.9, 14.3, 9.0, 3.9, 1.0]
-    }, {
-
-        lineColor: 'red',
-        data: [3.9, 4.2, 5.7, 8.5, 11.9, 15.2, 17.0, 16.6, 14.2, 10.3, 6.6, 4.8]
-    }]
+    const series =  dataPoints;
+    
 
     return (
         <div className="flex border-2 border-slate-300 rounded-md divide-x-2">
@@ -122,7 +79,7 @@ function Burndown({slug, sprints}:{slug: string, sprints:{id: string, value: str
                     </Select>
                 </div>
             </div>
-            {showChart ? <LineGraph name="Sprint Burndown" labels={[]} series={series} /> : <div className="flex-1 p-16 min-h-50">Loading...</div>}
+            {showChart ? <MultiLineGraph name="Sprint Burndown" labels={[]} series={series} /> : <div className="flex-1 p-16 min-h-50">Loading...</div>}
         </div>
     )
 }
