c5e2b5139cdaaddac16ccc1310fb44bf02fe7b65
AshankDsouza <ajdsouz1@asu.edu>
2024-03-24 17:59:27 -0700

    removes filters

--- client/components/burndown.tsx
+++ client/components/burndown.tsx
@@ -57,28 +57,6 @@ function Burndown({slug, sprints}:{slug: string, sprints:{id: string, value: str
 
     return (
         <div className="flex border-2 border-slate-300 rounded-md divide-x-2">
-            <div className="filters flex flex-col divide-y-2">
-                <div className="p-8 font-bold">Filters</div>
-                <div className="p-8">
-                    <Select onValueChange={(e) => setselectedSprintID(e)}>
-                        <SelectTrigger className="w-[180px]">
-                            <SelectValue placeholder="Sprint" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectGroup>
-                                <SelectLabel>Sprints</SelectLabel>
-                                {sprints && sprints.map(sprint =>
-                                    <>
-                                     <SelectItem key={sprint.id} value={sprint.id}>{sprint.value}</SelectItem>
-                             
-                                    </>
-                                    
-                                )}
-                            </SelectGroup>
-                        </SelectContent>
-                    </Select>
-                </div>
-            </div>
             {showChart ? <MultiLineGraph name="Sprint Burndown" labels={[]} series={series} /> : <div className="flex-1 p-16 min-h-50">Loading...</div>}
         </div>
     )
