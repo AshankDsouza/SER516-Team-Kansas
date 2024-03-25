80f0d67d72be0d56a5a5d38835375cfa403cfb4c
AshankDsouza <ajdsouz1@asu.edu>
2024-03-24 18:13:15 -0700

    deletes commented out code

--- client/actions/project.ts
+++ client/actions/project.ts
@@ -63,18 +63,9 @@ export async function getLeadTime(projectSlug: string, sprintId: string) {
 }
 
 export async function getBurndowMetrics(projectSlug: string) {
-    const Response = z.array(z.object({
-        // open_points: z.number()
-    }))
     const response = await fetch(`${process.env.API_URL}/api/${projectSlug}/multiSprintBundown`, getRequestOptions())
     let BurndownData = await response.json()
-    // try {
-    //     Response.parse(BurndownData)
-    // } catch (error) {
-    //     console.log({error});
-        
-    //     return null
-    // }
+
     return BurndownData
 }
 
