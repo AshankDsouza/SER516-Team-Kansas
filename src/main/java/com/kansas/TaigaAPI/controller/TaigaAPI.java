cc1e084c9fe92014d13ddef1b26581b9c33799d0
AshankDsouza <ajdsouz1@asu.edu>
2024-03-31 19:24:39 -0700

    deletes un needed files

--- src/main/java/com/kansas/TaigaAPI/controller/TaigaAPI.java
+++ src/main/java/com/kansas/TaigaAPI/controller/TaigaAPI.java
@@ -18,39 +18,4 @@ public class TaigaAPI {
         return "Hello";
     }
 
-    @GetMapping("/openUserStories")
-    public String getOpenUserStories() {
-        return "Open user stories data";
-    }
-
-    @GetMapping("/closedTasksPerWeek")
-    public String getClosedTasksPerWeek() {
-        return "Closed tasks per week data";
-    }
-
-    @GetMapping("/selectProjectsForMetrics")
-    public String[] getProjectsForMetrics(@RequestParam String metric) {
-        if (metric.equals("burndown")) {
-            String[] projects = {"nicolasrmz-devgym", "whrisurdicky-sim2023q2-spanners",
-                    "the-princess-bride", "a21asmchaben-project-hunt", "vitao-locacao-de-veiculos"};
-            return projects;
-        } else if (metric.equals("leadtime")) {
-            String[] projects = {"posh", "whrisurdicky-sim2023q2-spanners",
-                    "the-princess-bride", "a21asmchaben-project-hunt", "vitao-locacao-de-veiculos"};
-            return projects;
-
-        }
-        return null;
-    }
-
-    @GetMapping("/selectProjectsForVisulaisation")
-    public String getProjectForVisulaisation(@RequestParam String project) {
-        String[] selectedProjects = {"the-princess-bride", "melodicpinpon-z-anatomy", "transformap", "penpot", "viya-mdn-durable-team"};
-        for (int i = 0; i < selectedProjects.length; i++) {
-            if (project.equals(selectedProjects[i])) {
-                return "You have selected Project No: " + (i + 1) ;
-            }
-        }
-        return "Please choose a Project";
-    }
 }
