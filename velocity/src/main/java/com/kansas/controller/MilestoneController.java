ca47e4a359a70319984afd3b5065394f1b2978c7
Pratima <psing161@asu.edu>
2024-04-06 18:56:01 -0700

    apis added

---
+++ velocity/src/main/java/com/kansas/controller/MilestoneController.java
@@ -0,0 +1,258 @@
+package com.kansas.controller;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.kansas.model.*;
+import com.kansas.service.AuthenticationService;
+import com.kansas.service.MilestoneService;
+import com.kansas.service.ProjectService;
+import com.kansas.service.TasksService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.text.ParseException;
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api")
+public class MilestoneController {
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private MilestoneService milestoneService;
+
+    @Autowired
+    private TasksService tasksService;
+
+    @Autowired
+    private ProjectService projectService;
+
+    @GetMapping("/{milestoneId}/stats")
+    public JsonNode getBurnDownMetrics(@RequestHeader("Authorization") String authorizationHeader,
+            @PathVariable int milestoneId) {
+        return milestoneService.getBurnDownMetrics(authenticationService.getAuthToken(authorizationHeader),
+                milestoneId);
+    }
+
+    @GetMapping("/getMilestoneList")
+    public JsonNode getMilestoneList(@RequestHeader("Authorization") String authorizationHeader,
+            @RequestParam("project") int projectId) {
+        return milestoneService.getMilestoneList(authenticationService.getAuthToken(authorizationHeader), projectId);
+    }
+
+    @GetMapping("/getMilestoneListForSprint")
+    public JsonNode getMilestoneListForSprint(@RequestHeader("Authorization") String authorizationHeader,
+            @RequestParam("project") int projectId, @RequestParam("sprintNo") int sprintNo) {
+        JsonNode jsonNode = getMilestoneList(authorizationHeader, projectId);
+        int value = jsonNode.size() - sprintNo;
+        // System.out.println("Value " +value+" Sprint Length "+ jsonNode);
+        return jsonNode.get(value);
+    }
+
+    @GetMapping("/getAllSprints")
+    public HashMap<String, Integer> getAllSprints(@RequestHeader("Authorization") String authorizationHeader,
+            @RequestParam("project") String projectSlug) {
+        ProjectService projectService = new ProjectService();
+        int projectId = projectService.getProjectId(authenticationService.getAuthToken(authorizationHeader),
+                projectSlug);
+        JsonNode jsonNode = getMilestoneList(authorizationHeader, projectId);
+        HashMap<String, Integer> sprintVal = new HashMap<String, Integer>();
+        for (int i = 0; i < jsonNode.size(); i++) {
+            sprintVal.put(jsonNode.get(i).get("name").asText(), jsonNode.get(i).get("id").asInt());
+        }
+        return sprintVal;
+    }
+
+    // Burndown chart
+    @GetMapping("/{milestoneId}/getBurnDownChart")
+    public JsonNode getTotalStoryPoints(@RequestHeader("Authorization") String authorizationHeader,
+            @PathVariable int milestoneId) {
+        JsonNode milestoneData = milestoneService
+                .getMilestoneData(authenticationService.getAuthToken(authorizationHeader), milestoneId);
+        int storyPointsPending = milestoneData.get("total_points").asInt();
+
+        Map<String, Integer> storyPointsCompleted = new HashMap<>();
+        for (JsonNode userStory : milestoneData.get("user_stories")) {
+            String key;
+            if (userStory.has("finish_date") && !userStory.get("finish_date").isNull()) {
+                key = userStory.get("finish_date").toString().substring(0, 11) + '"';
+            } else {
+                key = "null";
+            }
+            int value = userStory.get("total_points").asInt();
+            if (storyPointsCompleted.containsKey(key) && key != "null") {
+                int existingSum = storyPointsCompleted.get(key);
+                storyPointsCompleted.put(key, existingSum + value);
+            } else {
+                storyPointsCompleted.put(key, value);
+            }
+        }
+        System.out.println(storyPointsCompleted);
+        // add a new field to the arrayNode to contain story points completed per day
+
+        JsonNode rootNode = milestoneService.getBurnDownMetrics(authenticationService.getAuthToken(authorizationHeader),
+                milestoneId);
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode arrayNode = mapper.createArrayNode();
+        if (rootNode.isArray()) {
+            for (JsonNode elementNode : rootNode) {
+                ObjectNode filteredObject = mapper.createObjectNode();
+                filteredObject.put("day", elementNode.get("day").asText());
+                filteredObject.put("open_points", elementNode.get("open_points").asDouble());
+                filteredObject.put("optimal_points", elementNode.get("optimal_points").asDouble());
+                if(storyPointsCompleted.containsKey(elementNode.get("day").toString())){
+                    storyPointsPending -= storyPointsCompleted.get(elementNode.get("day").toString());
+                }
+                filteredObject.put("story_points", storyPointsPending);
+                arrayNode.add(filteredObject);
+            }
+        }
+
+        return arrayNode;
+    }
+
+    // Cycle Time
+//    @GetMapping("/{projectSlug}/{milestoneId}/getCycleTime")
+//    public List<CycleTime> getCycleTime(@RequestHeader("Authorization") String authorizationHeader,
+//            @PathVariable String projectSlug, @PathVariable int milestoneId) {
+//        String authToken = authenticationService.getAuthToken(authorizationHeader);
+//        int projectId = projectService.getProjectId(authToken, projectSlug);
+//        return tasksService.getTaskHistory(projectId, milestoneId, authToken);
+//    }
+
+    @GetMapping("getDataForLeadTime")
+    public ArrayList getDataForLeadTime(@RequestHeader("Authorization") String authorizationHeader,
+            @RequestParam("projectSlug") String projectSlug, @RequestParam("sprintId") int sprintId)
+            throws ParseException {
+        int projectId = projectService.getProjectId(authenticationService.getAuthToken(authorizationHeader),
+                projectSlug);
+
+        ArrayList map = new ArrayList();
+        JsonNode jsonNode = getMilestoneList(authorizationHeader, projectId);
+        // Getting data from taiga api
+        JsonNode relData = getGivenSprintData(sprintId, jsonNode);
+        relData = relData.get("user_stories");
+        for (int i = 0; i < relData.size(); i++) {
+            int leadTime = 0;
+            int closedTasks = 0;
+            if (!relData.get(i).get("finish_date").isNull()) {
+
+                LocalDate createdDate = LocalDate.parse(relData.get(i).get("created_date").toString().substring(1, 11));
+                LocalDate finishDate = LocalDate.parse(relData.get(i).get("finish_date").toString().substring(1, 11));
+                String userStoryName = (relData.get(i).get("subject")).asText();
+                // leadTime += Duration.between(createdDate.toLocalDate().atStartOfDay(),
+                // finishDate.toLocalDate().atStartOfDay()).toDays();
+                HashMap hs = new HashMap();
+                hs.put("created_date", createdDate);
+                hs.put("finish_date", finishDate);
+                hs.put("userStory_Name", userStoryName);
+                // hs.put("lead_time",leadTime);
+                map.add(hs);
+            }
+        }
+        return map;
+    }
+
+    public JsonNode getGivenSprintData(int sprintId, JsonNode allSprintData) {
+        for (int i = 0; i < allSprintData.size(); i++) {
+            if (Integer.parseInt(allSprintData.get(i).get("id").toString()) == sprintId) {
+                return allSprintData.get(i);
+            }
+        }
+        return null;
+    }
+
+    @GetMapping("/{projectSlug}/getTotalPoints")
+    public List<TotalPoints> getMilestoneCompletedPoints(@RequestHeader("Authorization") String authorizationHeader,
+            @PathVariable String projectSlug) {
+        String authToken = authenticationService.getAuthToken(authorizationHeader);
+        int projectId = projectService.getProjectId(authToken, projectSlug);
+        return milestoneService.getMilestoneTotalPoints(authToken, projectId);
+
+    }
+
+    // Work Capacity
+    @GetMapping("/{projectSlug}/getCompletedPoints")
+    public List<CompletedPoints> getMilestoneTotalCompletedPoints(
+            @RequestHeader("Authorization") String authorizationHeader, @PathVariable String projectSlug) {
+        String authToken = authenticationService.getAuthToken(authorizationHeader);
+        int projectId = projectService.getProjectId(authToken, projectSlug);
+
+        return milestoneService.getMilestoneCompletedPoints(authToken, projectId);
+
+    }
+
+
+    @GetMapping("/{projectSlug}/multiSprintBundown")
+    public HashMap<String,ArrayNode> getmultiSprintBundown(@RequestHeader("Authorization") String authorizationHeader,@PathVariable String projectSlug){
+        String authToken = authenticationService.getAuthToken(authorizationHeader);
+        HashMap projectDetails = projectService.getprojectIdAndSprintId(authToken,projectSlug);
+        int projectId  = Integer.parseInt(projectDetails.keySet().iterator().next().toString());
+
+        return milestoneService.getMultiSprintBurndown(authToken,projectId, (JsonNode) projectDetails.get(projectId));
+
+    }
+
+    @GetMapping("/getLeadTimeForAbitraryTimeframe")
+
+    public ArrayList getLeadTimeForAbitraryTimeframe(@RequestHeader("Authorization") String authorizationHeader,@RequestParam("projectSlug") String projectSlug, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate) throws ParseException {
+        int projectId=projectService.getProjectId(authenticationService.getAuthToken(authorizationHeader), projectSlug);
+       
+
+        ArrayList map = new ArrayList();
+        JsonNode jsonNode = getMilestoneList(authorizationHeader, projectId);
+        LocalDate createDateFromString = LocalDate.parse(startDate);
+        LocalDate endDateFromString = LocalDate.parse(endDate);
+
+        for (int i = 0; i < jsonNode.size(); i++) {
+            int userStoriesCount = jsonNode.get(i).get("user_stories").size();
+
+            for (int j = 0; j < userStoriesCount; j++) {
+                JsonNode relData = jsonNode.get(i).get("user_stories");
+
+                LocalDate createdDate = LocalDate.parse(relData.get(j).get("created_date").toString().substring(1, 11));
+                if (relData.get(j).get("finish_date") != null) {
+                    LocalDate finishDate = LocalDate
+                            .parse(relData.get(j).get("finish_date").toString().substring(1, 11));
+                    if (!finishDate.isAfter(endDateFromString) && !createdDate.isBefore(createDateFromString)) {
+
+                    System.out.println("Fetching details");
+                        HashMap hs=new HashMap();
+                        hs.put("created_date", createdDate);
+                        hs.put("finish_date", finishDate);
+                        hs.put("userStory_Name", (relData.get(j).get("subject")).asText());
+                        hs.put("id",(relData.get(j).get("id")).asText());
+
+                        map.add(hs);
+                    }
+                }
+            }
+        }
+        return map;
+
+    }
+
+    @GetMapping("/{milestoneId}/getEstimateEffectiveness")
+    public List<EffectiveEstimatePoints> getEstimateEffectiveness(
+            @RequestHeader("Authorization") String authorizationHeader, @PathVariable int milestoneId) {
+        return tasksService.calculateEstimateEffectiveness(milestoneId,
+                authenticationService.getAuthToken(authorizationHeader));
+
+    }
+
+    @GetMapping("/{projectSlug}/getArbitraryCycleTime")
+    public List<ArbitaryCycleTime> getCycleTimeForArbitaryTimeFrame(@RequestHeader("Authorization") String authorizationHeader, @PathVariable String projectSlug, @RequestParam("startDate") String startDate, @RequestParam("endDate") String endDate){
+//        String authToken = authenticationService.getAuthToken(authorizationHeader);
+        int projectId = projectService.getProjectId(authenticationService.getAuthToken(authorizationHeader), projectSlug);
+        return tasksService.getCycleTimeForArbitaryTimeFrame(projectId, authenticationService.getAuthToken(authorizationHeader), startDate, endDate);
+    }
+
+}
---
+++ velocity/src/main/java/com/kansas/service/MilestoneService.java
@@ -0,0 +1,161 @@
+package com.kansas.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.kansas.model.CompletedPoints;
+import com.kansas.utils.GlobalData;
+import com.kansas.utils.HTTPRequest;
+import lombok.extern.log4j.Log4j2;
+import org.apache.http.HttpHeaders;
+import org.apache.http.client.methods.HttpGet;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import com.kansas.model.TotalPoints;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@Log4j2
+public class MilestoneService {
+
+    private static final String TAIGA_API_ENDPOINT = GlobalData.getTaigaURL();
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @Autowired
+    private ProjectService projectService;
+
+    private static final ObjectMapper objectMapper = new ObjectMapper()
+            .registerModule(new JavaTimeModule())
+            .configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
+
+    public JsonNode getBurnDownMetrics(String authToken,int mileStoneId) {
+        JsonNode responseJson = getMilestoneStats(authToken,mileStoneId);
+        return responseJson.get("days");
+    }
+
+    public JsonNode getMilestoneList(String authToken, int projectId) {
+
+        try {
+            String endpoint = TAIGA_API_ENDPOINT + "/milestones?project=" + projectId;
+            HttpGet request = new HttpGet(endpoint);
+            request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authToken);
+            request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+
+            String responseJson = HTTPRequest.sendHttpRequest(request);
+
+            return objectMapper.readTree(responseJson);
+        } catch(Exception e) {
+
+            // log.error(e.getMessage());
+
+            return null;
+        }
+    }
+
+    public List<TotalPoints>  getMilestoneTotalPoints(String authToken, int projectId){
+        JsonNode milestoneList = getMilestoneList(authToken,projectId);
+        List<TotalPoints> totalPointsList = new ArrayList<>();
+        for(JsonNode milestone: milestoneList){
+            if(milestone.get("id") != null){
+                JsonNode milestoneData = getMilestoneData(authToken, milestone.get("id").asInt());
+                int totalPoints = 0;
+                if(!milestoneData.get("total_points").isNull()){
+                    totalPoints = milestoneData.get("total_points").asInt();
+
+                }
+                totalPointsList.add(new TotalPoints(String.valueOf(milestone.get("name").asText()), totalPoints));
+            }
+        }
+        return totalPointsList;
+    }
+
+
+    public List<CompletedPoints> getMilestoneCompletedPoints(String authToken,int projectId){
+        JsonNode milestoneList = getMilestoneList(authToken,projectId);
+        List<CompletedPoints> completedPointsList = new ArrayList<>();
+        for(JsonNode milestone: milestoneList){
+            if(milestone.get("id") != null){
+                JsonNode milestoneStats = getMilestoneStats(authToken, milestone.get("id").asInt());
+                int completedPoints = 0;
+                if(milestoneStats.get("completed_points").isArray()){
+                    for(JsonNode points : milestoneStats.get("completed_points")) {
+                        completedPoints += points.asInt();
+                    }
+                }
+                completedPointsList.add(new CompletedPoints(String.valueOf(milestone.get("name").asText()), completedPoints));
+            }
+        }
+        return completedPointsList;
+    }
+
+    public JsonNode getMilestoneStats(String authToken, int milestoneId) {
+        try {
+            String endpoint = TAIGA_API_ENDPOINT + "/milestones/" + milestoneId + "/stats";
+            HttpGet request = new HttpGet(endpoint);
+            request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authToken);
+            request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+            return objectMapper.readTree(HTTPRequest.sendHttpRequest(request));
+        } catch (Exception e) {
+            //  log.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public JsonNode getMilestoneData(String authToken, int milestoneId){
+        try {
+            String endpoint = TAIGA_API_ENDPOINT + "/milestones/" + milestoneId;
+
+            HttpGet request = new HttpGet(endpoint);
+            request.setHeader(HttpHeaders.AUTHORIZATION, "Bearer " + authToken);
+            request.setHeader(HttpHeaders.CONTENT_TYPE, "application/json");
+            return objectMapper.readTree(HTTPRequest.sendHttpRequest(request));
+        } catch(Exception e) {
+            //  log.error(e.getMessage());
+            return null;
+        }
+    }
+
+    public HashMap<String,ArrayNode> getMultiSprintBurndown(String authToken, int projectId, JsonNode projectDetails){
+        HashMap<String,ArrayNode> allSprintBrnd = new HashMap<String,ArrayNode>();
+        ArrayNode burndownData = null;
+        for(JsonNode milestone: projectDetails){
+            if(milestone.get("id") != null){
+                JsonNode responseJson = getMilestoneStats(authToken,milestone.get("id").asInt());
+                burndownData = extractBurnDountDaycountWise(responseJson.get("days"));
+                allSprintBrnd.put(milestone.get("name").asText(),burndownData);
+            }
+        }
+        return allSprintBrnd;
+    }
+
+    public ArrayNode extractBurnDountDaycountWise(JsonNode daysData){
+        ObjectMapper mapper = new ObjectMapper();
+        ArrayNode arrayNode = mapper.createArrayNode();
+        int dayCount=0;
+        if (daysData.isArray()) {
+
+            for (JsonNode elementNode : daysData) {
+                dayCount+=1;
+                ObjectNode filteredObject = mapper.createObjectNode();
+                filteredObject.put("day", dayCount);
+                filteredObject.put("open_points", elementNode.get("open_points").asDouble());
+                filteredObject.put("optimal_points", elementNode.get("optimal_points").asDouble());
+                arrayNode.add(filteredObject);
+            }
+        }
+        return arrayNode;
+    }
+
+}
