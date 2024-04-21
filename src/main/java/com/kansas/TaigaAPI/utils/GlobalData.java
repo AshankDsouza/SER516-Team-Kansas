24a68aa064ed5afc2a16c2732c46cb9dc917961b
Chandrakanth Dhanunjai Chintala <cchinta2@asu.edu>
2024-04-21 16:09:57 -0700

    Adding Value AUC Microservice

--- src/main/java/com/kansas/TaigaAPI/utils/GlobalData.java
+++ src/main/java/com/kansas/TaigaAPI/utils/GlobalData.java
@@ -143,4 +143,21 @@ public class GlobalData {
                     return "http://localhost:8090";
                 }
         }
+
+    public static String getValueAucURL() {
+        try (InputStream input = Main.class.getClassLoader().getResourceAsStream("config.properties")) {
+            if (input == null) {
+                System.out.println("Sorry, unable to find config.properties");
+                return "default_taiga_url";
+            }
+
+            Properties properties = new Properties();
+            properties.load(input);
+            return properties.getProperty("VALUE_AUC_URL");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "http://localhost:8090";
+        }
+    }
     }
