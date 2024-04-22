eb78dca5ad8c22a125b2d06685f7203afa359d41
shashankreddyband <sbanda8@asu.edu>
2024-04-22 13:46:42 -0700

    Dashboard consilidation

--- client/components/dashboard.tsx
+++ client/components/dashboard.tsx
@@ -19,6 +19,15 @@ import LeadTimeArbitaryGraph from "./leadTimeArbitary"
 import BurndownMulti from "./burndownMulti"
 import AUCGraph from "./aucGraph";
 import ValueAUC from "./valueAUC";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
 
 function Dashboard({ slug }: { slug: string }) {
@@ -66,31 +75,43 @@ function Dashboard({ slug }: { slug: string }) {
       <Link href={"/project"}><div className="flex font-bold gap-2 underline"><ArrowLeft></ArrowLeft>{slug}</div></Link>
       <div className="flex justify-between mb-4">
         <div className="flex-wrap gap-6">
-        {charts.map(chart =>
-          <Button key={chart} onClick={() =>  {setChart(chart)}}>{chart}</Button>
-        )}
+          <Select onValueChange={(e) => setChart(e)}>
+            <SelectTrigger className="w-[180px]">
+              <SelectValue placeholder="Chart" />
+            </SelectTrigger>
+            <SelectContent>
+              <SelectGroup>
+                <SelectLabel>Charts</SelectLabel>
+                {charts && charts.map(chart =>
+                  <>
+                    <SelectItem key={chart} value={chart}>{chart}</SelectItem>
+
+                  </>
+
+                )}
+              </SelectGroup>
+            </SelectContent>
+          </Select>
         </div>
-      <Button onClick={()=>localStorage.clear()} className="flex gap-2"><RotateCcw size={16}/>Refresh cache</Button>
+        <Button onClick={() => localStorage.clear()} className="flex gap-2"><RotateCcw size={16} />Refresh cache</Button>
       </div>
 
-        {chart == "Burndown" ? <Burndown slug={slug} sprints={sprints} /> : <div key={"1"} className=" hidden"></div>}
-        {chart == "BurndownMulti" ? <BurndownMulti slug={slug} sprints={sprints} /> : <div key={"2"} className=" hidden"></div>}
-        {chart == "Lead time" ? <LeadTime slug={slug} sprints={sprints} /> : <div key={"3"} className=" hidden"></div>}
-        {chart == "Cycle time" ? <CycleTime slug={slug} sprints={sprints} /> : <div key={"4"} className=" hidden"></div>}
-        {chart == "Focus Factor" ? <FocusFactor slug={slug} sprints={sprints} /> : <div key={"5"} className=" hidden"></div>}
-        {chart == "Velocity" ? <VelocityGraph slug={slug} sprints={sprints} /> : <div key={"6"} className=" hidden"></div>}
-        {chart == "Estimate Effectiveness" ? <EstimateEffectiveness slug={slug} sprints={sprints} /> : <div key={"7"} className=" hidden"></div>}
-        {chart == "Work Capacity" ? <WorkCapacityGraph slug={slug} /> : <div key={"8"} className="hidden"></div>}
-        {chart == "LeadTime Arbitary" ? <LeadTimeArbitaryGraph slug={slug} sprints={sprints} /> : <div key={"9"} className=" hidden"></div>}
-        {chart == "Arbitary Cycle Time" ? (<ArbitaryCycleTimeGraph slug={slug} />) : (<div key={"10"} className=" hidden"></div>)}
-        {chart == "Value In Progress" ? (<ValueInProgress slug={slug} sprints={sprints} />) : (<div key={"10"} className=" hidden"></div>)}
-        {chart == "Work AUC" ? <AUCGraph slug={slug} sprints={sprints} ></AUCGraph> : (<div key={"10"} className=" hidden"></div>)}
-        {chart == "Value AUC" ? <ValueAUC slug={slug} sprints={sprints} ></ValueAUC> : (<div key={"10"} className=" hidden"></div>)}
-      </div>
-
+      {chart == "Burndown" ? <Burndown slug={slug} sprints={sprints} /> : <div key={"1"} className=" hidden"></div>}
+      {chart == "BurndownMulti" ? <BurndownMulti slug={slug} sprints={sprints} /> : <div key={"2"} className=" hidden"></div>}
+      {chart == "Lead time" ? <LeadTime slug={slug} sprints={sprints} /> : <div key={"3"} className=" hidden"></div>}
+      {chart == "Cycle time" ? <CycleTime slug={slug} sprints={sprints} /> : <div key={"4"} className=" hidden"></div>}
+      {chart == "Focus Factor" ? <FocusFactor slug={slug} sprints={sprints} /> : <div key={"5"} className=" hidden"></div>}
+      {chart == "Velocity" ? <VelocityGraph slug={slug} sprints={sprints} /> : <div key={"6"} className=" hidden"></div>}
+      {chart == "Estimate Effectiveness" ? <EstimateEffectiveness slug={slug} sprints={sprints} /> : <div key={"7"} className=" hidden"></div>}
+      {chart == "Work Capacity" ? <WorkCapacityGraph slug={slug} /> : <div key={"8"} className="hidden"></div>}
+      {chart == "LeadTime Arbitary" ? <LeadTimeArbitaryGraph slug={slug} sprints={sprints} /> : <div key={"9"} className=" hidden"></div>}
+      {chart == "Arbitary Cycle Time" ? (<ArbitaryCycleTimeGraph slug={slug} />) : (<div key={"10"} className=" hidden"></div>)}
+      {chart == "Value In Progress" ? (<ValueInProgress slug={slug} sprints={sprints} />) : (<div key={"11"} className=" hidden"></div>)}
+      {chart == "Work AUC" ? <AUCGraph slug={slug} sprints={sprints} ></AUCGraph> : (<div key={"12"} className=" hidden"></div>)}
+      {chart == "Value AUC" ? <ValueAUC slug={slug} sprints={sprints} ></ValueAUC> : (<div key={"13"} className=" hidden"></div>)}
     </div>
   );
 }
-       
+
 
 export default Dashboard;
