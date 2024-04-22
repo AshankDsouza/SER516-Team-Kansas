d814b35119044195082b2e05a7e8cab5bbe1193f
SukanyaRani <srani6@asu.edu>
2024-04-21 19:26:42 -0700

    Integrating Ui and backend

--- client/components/valueAUC.tsx
+++ client/components/valueAUC.tsx
@@ -17,17 +17,17 @@ import Chip from '@mui/material/Chip';
 
 
 
-function ValueAUC({slug, sprints}:{slug: string, sprints:{id: string, value: string}[]}) {
+function ValueAUC({slug, sprints}:{slug: string, sprints:{id: string, Value: string}[]}) {
 
     const router = useRouter();
 
     const [selectedSprintID, setselectedSprintID] = useState("")
     const [showChart, setShowChart] = useState(true)
     const [labels, setLabels] = useState<string[]>([])
-    const [value, setValue] = useState<number[]>([])
+    const [Value, setValue] = useState<number[]>([])
     const [bval, set_bv] = useState<number[]>([])
 
-    let sprintNames = sprints.map((sprint) => sprint.value);
+    let sprintNames = sprints.map((Sprint) => Sprint.Value);
 
     useEffect(() => {
         getValueAUC(slug,selectedSprintID)
@@ -37,9 +37,9 @@ function ValueAUC({slug, sprints}:{slug: string, sprints:{id: string, value: str
                     return
                 }
                 setShowChart(false);
-                const daysArray = data.map((item: any) => item.sprint);
+                const daysArray = data.map((item: any) => item.Sprint);
                 setLabels(daysArray);
-                const value = data.map((item: any) => item.value);
+                const value = data.map((item: any) => item.Value);
                 setValue(value);
                 setShowChart(true);
             })
@@ -52,7 +52,7 @@ function ValueAUC({slug, sprints}:{slug: string, sprints:{id: string, value: str
 
             {
                 label: 'value',
-                data: value,
+                data: Value,
                 borderColor: '#666',
                 backgroundColor: '#666',
             }
@@ -63,25 +63,7 @@ function ValueAUC({slug, sprints}:{slug: string, sprints:{id: string, value: str
         <div className="flex border-2 border-slate-300 rounded-md divide-x-2">
             <div className="filters flex flex-col divide-y-2">
                 <div className="p-8 font-bold">Filters</div>
-                <div className="p-8">
-                    <Select onValueChange={(e) => setselectedSprintID(e)}>
-                        <SelectTrigger className="w-[180px]">
-                            <SelectValue placeholder="Sprint" />
-                        </SelectTrigger>
-                        <SelectContent>
-                            <SelectGroup>
-                                <SelectLabel>Sprints</SelectLabel>
-                                {sprints && sprints.map(sprint =>
-                                    <>
-                                     <SelectItem key={sprint.id} value={sprint.id}>{sprint.value}</SelectItem>
-
-                                    </>
-
-                                )}
-                            </SelectGroup>
-                        </SelectContent>
-                    </Select>
-                </div>
+                
             </div>
             {showChart ? <BurndownChart data={data} /> : <div className="flex-1 p-16 min-h-50">Loading...</div>}
         </div>
